39e63bc55cbe2359f98c16ae8dc91ebd362ca4e6
hong-gyeonghyeon <wertys5674@naver.com>
2021-04-16 14:55:02 +0900

    try-catch, exception printStackTrace()

--- src/practicejava/mainclass.java
+++ src/practicejava/mainclass.java
@@ -8,9 +8,11 @@ class Myclass {
             System.out.println(0 / 0); //문제 발생, 바로 catch 문으로 이동.
             System.out.println(2);
         } catch (ArithmeticException ae) {
-            System.out.println(3);
-        } catch (Exception e){ //ArithmeticException 을 제외한 모든 Exception 발생 시 실행된다.
-            System.out.println("Exception occurs!");
+            if (ae instanceof ArithmeticException) //ae 가 ArithmeticException 을 상속받는 클래스나, 여기에 속해있다면 true;
+                System.out.println("ArithmeticException Ocuurs");
+            ae.printStackTrace();
+        } catch (Exception e) { //ArithmeticException 을 제외한 모든 Exception 발생 시 실행된다.
+            System.out.println("Other Exception Occurs!");
         }
 
     }
