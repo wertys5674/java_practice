869ea8abb72a8162a0436496abcbc5889a202dfa
hong-gyeonghyeon <wertys5674@naver.com>
2021-04-19 15:14:44 +0900

    practicing variable method StringBuffer etc

--- src/practicejava/mainclass.java
+++ src/practicejava/mainclass.java
@@ -45,9 +45,9 @@ class Myclass {
 
         //String  비교하기
         String str1 ="abc";
-        String str2 ="abc";
-        String str3 =new String("abc");
-        String str4 =new String("abc");
+        String str2 ="abc";                     //이렇게 하는게 보통.
+        String str3 =new String("HelloImKH");
+        String str4 =new String("HelloImKH");  //굳이 이렇게 하지 말자. 두 개가 다름.
 
         if(str1==str2)
             System.out.println("same");
@@ -62,5 +62,18 @@ class Myclass {
 
         System.out.println(str1.equals(str2));
         System.out.println(str3.equals(str4));
+        // String method
+        System.out.println(str3.charAt(6)); //문자열의 6번째 문자 반환
+        char[] ch ={'K','i','n','g','h','y','u','n'};
+        String str5 = new String(ch);
+        System.out.println(str5);
+        System.out.println(str1.equalsIgnoreCase(str2));
+        System.out.println(str4.indexOf('l'));
+        str3 = "new string";
+        System.out.println(str3); // 기존 str3 에 있던 문자열은 그대로 남아있고, 새로운 문자열이 생겨서 그 주소를 str3이 가리키는 구조.
+        StringBuffer sb = new StringBuffer("This is String Buffer");
+        System.out.println(sb);
+        sb.append(3);
+        System.out.println(sb);
     }
 }
